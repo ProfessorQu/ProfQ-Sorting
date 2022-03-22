258eb903319c5aed038c7417d013f8444f12dd0a
ProfessorQu <quint@dasookwat.com>
2022-03-22 16:17:07 +0100

    Added Insertion Sort
    Implemented insertion sort.
    Added comments to bubble sort.

--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -9,7 +9,11 @@ def bubble_sort(arr: list):
     """
     size = len(arr)
 
+    # Loop over the entire list
     for _ in range(1, size):
+        # Loop over the list to swap
         for j in range(0, size - 1):
+            # Swap items if the current selected is greater than the next
             if arr[j] > arr[j + 1]:
+                # Swap
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
